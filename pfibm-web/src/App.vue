2baa3aca5831a4c617b3968f3b5b6a36a37adc00
Jakob <gh.2019-j64>
2022-05-17 16:32:49 +0200

    initial commit

---
+++ pfibm-web/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <VueSidebarMenuAkahon menu-title="pfIBM" profileImg="" profileName="ToDo: Username"
+                          profileRole="" :menu-items="getMenuItems()"/>
+    <MainContent title="Test"></MainContent>
+  </div>
+</template>
+
+<script>
+import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon"
+import MainContent from "./components/MainContent";
+
+export default {
+  name: 'App',
+  components: {
+    MainContent,
+    VueSidebarMenuAkahon
+  },
+  methods:{
+    getMenuItems(){
+      return [{link: '/test',name: 'Dashboard', tooltip: 'Dashboard', icon: 'bx-grid-alt' }]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 2em;
+  color: #2c3e50;
+}
+</style>
